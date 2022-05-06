8736770bdaf4509b420c841ee501bee5f7116674
Mateusz Zagórski <01161597@pw.edu.pl>
2022-05-06 18:03:30 +0200

    Finished whole tutorial

---
+++ ColorPreview/ColorInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ColorPreview
+{
+    class ColorInfo
+    {
+        public string Name { get; set; }
+        public string RgbInfo { get; set; }
+        public Color Rgb { get; set; }
+    }
+    class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Color))
+                return null;
+            return new SolidColorBrush((Color)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- ColorPreview/MainWindow.xaml.cs
+++ ColorPreview/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reflection;
 
 namespace ColorPreview
 {
@@ -23,6 +24,21 @@ namespace ColorPreview
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var props = typeof(Colors).GetProperties(BindingFlags.Static | BindingFlags.Public);
+            var colorInfos = props.Select(prop =>
+            {
+                var color = (Color)prop.GetValue(null, null);
+                return new ColorInfo()
+                {
+                    Name = prop.Name,
+                    Rgb = color,
+                    RgbInfo = $"R:{color.R} G:{color.G}, B:{color.B}"
+                };
+            });
+            this.DataContext = colorInfos;
         }
     }
 }
