026d2485260ba6b7bc50614a3bb853b04d111308
Mateusz Zagórski <01161597@pw.edu.pl>
2022-05-06 16:42:16 +0200

    Add project files.

---
+++ ColorPicker/SlidersToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorPicker
+{
+    class SlidersToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type tergetType, object parameter, CultureInfo culture)
+        {
+            var red = System.Convert.ToByte(values[0]);
+            var green = System.Convert.ToByte(values[1]);
+            var blue = System.Convert.ToByte(values[2]);
+        return new SolidColorBrush(Color.FromArgb(255, red, green, blue));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
